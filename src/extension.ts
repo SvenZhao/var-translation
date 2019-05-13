51dc48ad361379e2a3da429fed2bc084ed6752ec
sven <wb-zb298945@alibaba-inc.com>
2019-05-13 16:58:00 +0800

    refactor: 调整代码结构

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import { window, ExtensionContext, commands, QuickPickItem, QuickPickOptions, workspace } from 'vscode';
 import { google, youdao, baidu } from 'translation.js';
 import { camelCase, paramCase, pascalCase, snakeCase, constantCase } from 'change-case';
+import { TranslateResult } from 'translation.js/declaration/api/types';
 
 export function activate(context: ExtensionContext) {
 	const disposable = commands.registerCommand('extension.varTranslation', vscodeTranslate);
@@ -8,6 +9,49 @@ export function activate(context: ExtensionContext) {
 }
 
 export function deactivate() { }
+
+async function vscodeTranslate() {
+	//获取编辑器
+	const editor = window.activeTextEditor;
+	if (!editor) { return; }
+	//获取选中文字
+	const selection = editor.selection;
+	let srcText = editor.document.getText(selection);
+	if (!srcText) { return; }
+	try {
+		const engine = getTheTranslationEngine();
+		const lang = await engine.detect(srcText);
+		//非英语需要翻译
+		if (lang !== 'en') {
+			const translationResult: TranslateResult = await engine.translate({ text: srcText, from: lang, to: 'en' });
+			if (translationResult && translationResult.result) { srcText = translationResult.result[0]; }
+		}
+		const result = await Select(srcText);
+		if (!result) { return; }
+		//替换文案
+		editor.edit(builder => builder.replace(selection, result));
+	}
+	catch (err) {
+		window.showInformationMessage('some thing error; maybe Network Error');
+	}
+}
+/**
+ * 获取用户选择结果
+ * @param {string}word 需要转换的单词 
+ * @return {string}转换结果
+ */
+async function Select(word: string) {
+	var items: QuickPickItem[] = [];
+	var opts: QuickPickOptions = { matchOnDescription: true, placeHolder: 'choose replace 选择替换' };
+	items.push({ label: camelCase(word), description: 'camelCase 小驼峰' });
+	items.push({ label: pascalCase(word), description: 'pascalCase 大驼峰' });
+	items.push({ label: snakeCase(word), description: 'snakeCase 下划线' });
+	items.push({ label: paramCase(word), description: 'paramCase 中划线' });
+	items.push({ label: constantCase(word), description: 'constantCase 常量' });
+	const selections = await window.showQuickPick(items, opts);
+	if (!selections) { return; }
+	return selections.label;
+}
 /**
  * 获取翻译引擎配置
  */
@@ -18,42 +62,4 @@ function getTheTranslationEngine() {
 	if (translationEngine === 'youdao') { return youdao; }
 	if (translationEngine === 'baidu') { return baidu; }
 	return google;
-}
-async function vscodeTranslate() {
-	const editor = window.activeTextEditor;
-	if (!editor) { return; }
-	const selection = editor.selection;
-	let srcText = editor.document.getText(selection);
-	if (!srcText) { return; }
-	let result: any;
-
-	try {
-		const engine = getTheTranslationEngine();
-		const lang = await engine.detect(srcText);
-		// 检查英语跳过
-		if (lang !== 'en') {
-			//	翻译内容
-			result = await engine.translate({ text: srcText, from: lang, to: 'en' });
-			result = String(result.result[0]);
-		} else { result = srcText; }
-		result = await Select(result);
-		if (!result) { return; }
-		//替换文案
-		editor.edit(builder => builder.replace(selection, result));
-	}
-	catch (err) {
-		window.showInformationMessage('some thing error; maybe Network Error');
-	}
-}
-async function Select(result: string) {
-	var items: QuickPickItem[] = [];
-	var opts: QuickPickOptions = { matchOnDescription: true, placeHolder: 'choose replace 选择替换' };
-	items.push({ label: camelCase(result), description: 'camelCase 小驼峰' });
-	items.push({ label: pascalCase(result), description: 'pascalCase 大驼峰' });
-	items.push({ label: snakeCase(result), description: 'snakeCase 下划线' });
-	items.push({ label: paramCase(result), description: 'paramCase 中划线' });
-	items.push({ label: constantCase(result), description: 'constantCase 常量' });
-	const selections = await window.showQuickPick(items, opts);
-	if (!selections) { return; }
-	return selections.label;
 }
